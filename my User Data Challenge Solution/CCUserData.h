6cee3b9e8d7b624f93d3e10258a70b70cc7b98a8
Admin <admin@Macintosh.local>
2014-10-08 18:04:56 +0700

    first commit

---
+++ my User Data Challenge Solution/CCUserData.h
@@ -0,0 +1,13 @@
+//
+//  CCUserData.h
+//  my User Data Challenge Solution
+//
+//  Created by Admin on 10/5/2557 BE.
+//  Copyright (c) 2557 Bonaliza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCUserData : NSObject
+
+@end
---
+++ my User Data Challenge Solution/CCUserData.m
@@ -0,0 +1,13 @@
+//
+//  CCUserData.m
+//  my User Data Challenge Solution
+//
+//  Created by Admin on 10/5/2557 BE.
+//  Copyright (c) 2557 Bonaliza. All rights reserved.
+//
+
+#import "CCUserData.h"
+
+@implementation CCUserData
+
+@end
